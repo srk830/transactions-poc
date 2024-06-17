74329a660a6f200bdb9654eec985d0938121fb01
vontelashiva.reddy <shivaramakrishnareddyv@gmail.com>
2024-06-18 02:20:01 +0530

    minor refactor

--- src/main/java/org/example/persistence1/config/PersistenceConfig1.java
+++ src/main/java/org/example/persistence1/config/PersistenceConfig1.java
@@ -1,8 +1,10 @@
 package org.example.persistence1.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import org.example.constants.Constants;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -20,9 +22,18 @@ import java.util.Properties;
         transactionManagerRef = Constants.TRANSACTION_MANAGER_1)
 public class PersistenceConfig1 {
 
+    @Bean(name = Constants.DATA_SOURCE_1)
+    @ConfigurationProperties(prefix = "spring.datasource1")
+    public DataSource dataSource1() {
+        return new HikariDataSource();
+    }
+
     @Bean(name = Constants.ENTITY_MANAGER_FACTORY_1)
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory1(@Qualifier(Constants.DATA_SOURCE_1) DataSource dataSource,
-                                                                        @Qualifier("properties") Properties properties) {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory1(@Qualifier(Constants.DATA_SOURCE_1) DataSource dataSource) {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("use_jdbc_metadata_defaults", "false");
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         factory.setJpaProperties(properties);
--- src/main/java/org/example/persistence2/config/PersistenceConfig2.java
+++ src/main/java/org/example/persistence2/config/PersistenceConfig2.java
@@ -1,8 +1,10 @@
 package org.example.persistence2.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import org.example.constants.Constants;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -20,9 +22,18 @@ import java.util.Properties;
         transactionManagerRef = Constants.TRANSACTION_MANAGER_2)
 public class PersistenceConfig2 {
 
+    @Bean(name = Constants.DATA_SOURCE_2)
+    @ConfigurationProperties(prefix = "spring.datasource2")
+    public DataSource dataSource2() {
+        return new HikariDataSource();
+    }
+
     @Bean(name = Constants.ENTITY_MANAGER_FACTORY_2)
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory1(@Qualifier(Constants.DATA_SOURCE_2) DataSource dataSource,
-                                                                        @Qualifier("properties") Properties properties) {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory1(@Qualifier(Constants.DATA_SOURCE_2) DataSource dataSource) {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("use_jdbc_metadata_defaults", "false");
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         factory.setJpaProperties(properties);
